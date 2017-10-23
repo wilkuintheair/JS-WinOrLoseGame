b6d82cf5fa5149c7bfeb92ce0d1ad85eae464cc8
Piotr Wilk <piotrekwilk1@gmail.com>
2017-10-23 02:13:40 +0200

    Added game's win lose mechanism

--- js/Game.js
+++ js/Game.js
@@ -8,8 +8,11 @@ class Game {
         this.container = new DisplayObjectContainer(this.context);
         this.frame = 0;
         this.animateColumns = false;
+        this.patternGenerator = new PatternHelper(0, images.length, 3, 3);
+        this.startPattern = [[0, 1, 2], [0, 1, 2], [0, 1, 2]];
+        this.finishPattern = this.patternGenerator.getRandomPattern();
         this.setupUI(ui);
-        this.setupImages(images);
+        this.createColumns(images);
         this.canvas.addEventListener("click", (event) => {
             this.container.click(event.clientX, event.clientY);
         });
@@ -18,37 +21,44 @@ class Game {
     setupUI(ui) {
         this.container.addChild(ui);
         ui.spinButton.onclick = () => {
-            this.animateColumns = !this.animateColumns;
-            ui.spinButtonEnabled = !ui.spinButtonEnabled;
+            if (ui.spinButtonEnabled) {
+                this.start();
+            }
         }
     }
 
-    setupImages(images) {
+    createColumns() {
         this.column1 = new DisplayObjectContainer(this.context);
         this.column1.x = 80;
         this.column1.y = 20;
-        this.createColumn(this.column1, images);
+        this.createColumn(this.column1, this.startPattern[0], this.finishPattern[0]);
         this.container.addChild(this.column1);
 
         this.column2 = new DisplayObjectContainer(this.context);
         this.column2.x = 320;
         this.column2.y = 20;
-        this.createColumn(this.column2, images);
+        this.createColumn(this.column2, this.startPattern[1], this.finishPattern[1]);
         this.container.addChild(this.column2);
 
         this.column3 = new DisplayObjectContainer(this.context);
         this.column3.x = 560;
         this.column3.y = 20;
-        this.createColumn(this.column3, images);
+        this.createColumn(this.column3, this.startPattern[2], this.finishPattern[2]);
         this.container.addChild(this.column3);
     }
 
-    createColumn(container, images) {
+    createColumn(container, startPattern, finishPattern) {
+        this.addImage(container, this.images[startPattern[0]]);
+        this.addImage(container, this.images[startPattern[1]]);
+        this.addImage(container, this.images[startPattern[2]]);
         for (let i = 0; i < 30; i++) {
-            let random = Math.floor(Math.random() * images.length);
-            let image = images[random];
+            let random = Math.floor(Math.random() * this.images.length);
+            let image = this.images[random];
             this.addImage(container, image);
         }
+        this.addImage(container, this.images[finishPattern[0]]);
+        this.addImage(container, this.images[finishPattern[1]]);
+        this.addImage(container, this.images[finishPattern[2]]);
     }
 
     addImage(container, image) {
@@ -67,6 +77,21 @@ class Game {
         }, this.FPS);
     }
 
+    reset() {
+        this.ui.spinButtonEnabled = true;
+        this.startPattern = this.finishPattern;
+        this.finishPattern = this.patternGenerator.getWinPattern();
+        this.container.removeChild(this.column1);
+        this.container.removeChild(this.column2);
+        this.container.removeChild(this.column3);
+        this.createColumns();
+    }
+
+    start() {
+        this.animateColumns = true;
+        this.ui.spinButtonEnabled = false;
+    }
+
     gameLoop() {
         this.frame++;
         this.clear();
@@ -79,10 +104,18 @@ class Game {
     }
 
     update() {
+        let minY = this.canvas.height - this.column1.height - 20;
         if (this.animateColumns) {
-            this.column1.y -= 20;
-            this.column2.y -= 20;
-            this.column3.y -= 20;
+            this.column1.y -= 50;
+            this.column2.y -= 50;
+            this.column3.y -= 50;
+        }
+        if (this.animateColumns && this.column1.y < minY) {
+            this.animateColumns = false;
+            this.column1.y = minY;
+            this.column2.y = minY;
+            this.column3.y = minY;
+            this.reset();
         }
         this.container.update();
     }
@@ -100,10 +133,10 @@ class GameUI extends DisplayObjectContainer {
         this._spinButtonDefault = new ImageDisplayObject(context, spinButtonDefault);
         this._spinButtonDisabled = new ImageDisplayObject(context, spinButtonDisabled);
         this._spinButton = new DisplayObjectContainer(context);
-        this._spinButtonEnabled = false;
+        this._spinButtonEnabled = true;
         this.spinButton.x = 824;
         this.spinButton.y = 218;
-        this.spinButton.addChild(this._spinButtonDisabled);
+        this.spinButton.addChild(this._spinButtonDefault);
         this.addChild(this.background);
         this.addChild(this.spinButton);
     }
@@ -133,5 +166,68 @@ class GameUI extends DisplayObjectContainer {
             this.spinButton.addChild(this._spinButtonDisabled);
         }
         this._spinButtonEnabled = value;
+    }
+}
+
+class PatternHelper {
+    constructor(winIndex, maxIndex, rows, columns) {
+        this.winIndex = winIndex;
+        this.maxIndex = maxIndex;
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    getRandomImageIndex() {
+        return Math.floor(Math.random() * this.maxIndex);
+    }
+
+    getRandomPattern() {
+        let pattern = [];
+        for (let i = 0; i < this.columns; i++) {
+            pattern.push([]);
+            for (let j = 0; j < this.rows; j++) {
+                pattern[i].push(this.getRandomImageIndex());
+            }
+        }
+        return pattern;
+    }
+
+    getLosePattern() {
+        let pattern = this.getRandomPattern();
+        while (this.isWinPattern(pattern)) {
+            pattern = this.getRandomPattern();
+        }
+        return pattern;
+    }
+
+    getWinPattern() {
+        let pattern = this.getLosePattern();
+        pattern[0][1] = this.winIndex;
+        pattern[1][1] = this.winIndex;
+        pattern[2][1] = this.winIndex;
+        return pattern;
+    }
+
+    isWinPattern(pattern) {
+        let isWin = false;
+        for (let i = 0; i < this.rows; i++) {
+            if (this.compareRow(pattern, i)) {
+                isWin = true;
+                break;
+            }
+        }
+        return isWin;
+    }
+
+    compareRow(pattern, row) {
+        let isWin = true;
+        let compareValue = pattern[0][row];
+        for (let i = 1; i < this.columns; i++) {
+            if (pattern[i][row] !== compareValue) {
+                isWin = false;
+                break;
+            }
+        }
+        return isWin;
     }
 }
